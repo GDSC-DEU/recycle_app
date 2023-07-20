2813f04b1b4ff64c7dd5eb4fa489df06abfbd640
YeJun, Jung <31740224+yejun614@users.noreply.github.com>
2023-07-21 03:19:43 +0900

    Intro Screen 추가 (#9)
    * Feat: add intro layout
    
    * Feat: add styles in intro layout
    
    * Feat: add bgImageAsset properties for IntroLayout
    
    * Feat: add sliding animation

---
+++ lib/layouts/intro_layout.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class IntroLayout extends StatefulWidget {
+  final double height;
+  final String text;
+  final String bgImageAsset;
+  final double? fontSize;
+
+  const IntroLayout({
+    super.key,
+    required this.height,
+    required this.text,
+    required this.bgImageAsset,
+    this.fontSize,
+  });
+
+  @override
+  State<IntroLayout> createState() => _IntroLayoutState(); 
+}
+
+class _IntroLayoutState extends State<IntroLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Image.asset(
+            widget.bgImageAsset,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Center(
+          child: Container(
+            width: double.infinity,
+            height: widget.height,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.black54,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Center(
+              child: Text(
+                widget.text,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: widget.fontSize,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
