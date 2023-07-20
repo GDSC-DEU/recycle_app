6e0b37ce611f23b4a9605b3c582d4c28af3b472f
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-07-21 08:48:11 +0900

    Feat: add contents Drawer

--- lib/screens/contents_screen.dart
+++ lib/screens/contents_screen.dart
@@ -15,20 +15,131 @@ class ContentsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight((65.0)),
-        child:  AppBar(
-          title: Text(text),
-          leading: const IconButton(icon: Icon(Icons.menu), onPressed: null, iconSize: 30,),
-          actions: const [
-            IconButton(icon: Icon(Icons.account_circle), onPressed: null, iconSize: 30, color: Colors.black,),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(text),
+        centerTitle: true,
+        elevation: 0,
+        actions: const [
+          IconButton(
+            icon: Icon(Icons.account_circle, color: Colors.black,),
+            onPressed: null, iconSize: 30
+          )
+        ],
+        titleTextStyle: const TextStyle(
+          color: Colors.black,
+          fontSize: 22,
+        ),
+        backgroundColor: Colors.white,
+
+      ),
+      drawer: Drawer(
+        backgroundColor: Color(0XffF7F2FA),
+        child: ListView(
+          padding: EdgeInsets.zero,
+
+          children: <Widget>[
+            SizedBox(
+              height: 50,
+              child: DrawerHeader(
+                child: Text("Title"),
+
+              ),
+            ),
+            ListBody(
+              children: [
+                ListTile(
+                  title: Text("SectionHeader"),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text("100+"),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text("100+"),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text("100+"),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text("100+"),
+                ),
+                ListTile(
+                  title: Text("SectionHeader"),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text(""),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text(""),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text(""),
+                ),
+                ListTile(
+                  title: Text("SectionHeader"),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text(""),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text(""),
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 7 ,
+                    backgroundColor: Colors.black,
+                  ),
+                  title: Text("Label"),
+                  trailing: Text(""),
+                ),
+              ],
+            )
           ],
-          titleTextStyle: const TextStyle(
-            color: Colors.black,
-            fontSize: 22,
-          ),
-          backgroundColor: Colors.white,
-          elevation: 0,
         ),
       ),
       body: ContentsListComponent(),
