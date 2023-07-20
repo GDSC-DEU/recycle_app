e8c90a71133bdf7ac08adb51e6ac2881a6fecb1c
YeJun, Jung <yejun614@naver.com>
2023-07-20 23:00:29 +0900

    Feat: add intro layout

---
+++ lib/layouts/intro_layout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IntroLayout extends StatefulWidget {
+  const IntroLayout({ super.key });
+
+  @override
+  State<IntroLayout> createState() => _IntroLayoutState(); 
+}
+
+class _IntroLayoutState extends State<IntroLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Test');
+  }
+}
